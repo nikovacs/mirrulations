b3370ec1e817755058d2c97068fb756c9824e7bb
Nikolas Kovacs <78886981+nikovacs@users.noreply.github.com>
2023-04-12 15:16:03 -0400

    add tests for extraction queue

--- mirrulations-extractor/tests/test_extractor.py
+++ mirrulations-extractor/tests/test_extractor.py
@@ -1,11 +1,20 @@
 from mirrextractor.extractor import Extractor
 import pikepdf
+from mirrmock.mock_rabbitmq import MockRabbit
 
 
 def mock_pdf_extraction(mocker):
+    mocker.patch('pikepdf.open', return_value=pikepdf.Pdf.new())
+    mocker.patch('pikepdf.Pdf.save', return_value=None)
+    mocker.patch('pdfminer.high_level.extract_text', return_value='test')
+    mocker.patch('os.makedirs', return_value=None)
+    mocker.patch("builtins.open", mocker.mock_open())
+
+
+def mock_text_extraction(mocker):
     mocker.patch.object(
         Extractor,
-        '_extract_pdf',
+        'extract_text',
         return_value=None
     )
 
@@ -52,3 +61,40 @@ def test_extract_pdf(mocker, capfd):
     mocker.patch("builtins.open", mocker.mock_open())
     Extractor.extract_text('a.pdf', 'b.txt')
     assert "SUCCESS: Saved extraction at" in capfd.readouterr()[0]
+
+
+def test_extract_queue_add_get_job():
+    extractor = Extractor()
+    extractor.extract_queue = MockRabbit()
+    extractor.extract_queue.add('test')
+    assert extractor.extraction_queue_size == 1
+    assert extractor.extract_queue.get() == 'test'
+
+
+def test_extract_next_in_queue(mocker, capfd):
+    extractor = Extractor()
+    extractor.extract_queue = MockRabbit()
+    extractor.extract_queue.add('a.pdf')
+    mocker.patch('mirrcore.path_generator.'
+                 'PathGenerator.make_attachment_save_path',
+                 return_value="b.pdf")
+    mock_pdf_extraction(mocker)
+    extractor.extract_next_in_queue()
+    assert "SUCCESS: Saved extraction at" in capfd.readouterr()[0]
+
+
+def test_extract_next_in_queue_queue_is_empty_nothing_happens(mocker, capfd):
+    extractor = Extractor()
+    extractor.extract_queue = MockRabbit()
+    mock_pdf_extraction(mocker)
+    extractor.extract_next_in_queue()
+    assert capfd.readouterr()[0] == ''
+
+
+def test_extract_next_in_queue_job_is_none_nothing_happens(mocker, capfd):
+    extractor = Extractor()
+    extractor.extract_queue = MockRabbit()
+    extractor.extract_queue.add(None)
+    mock_pdf_extraction(mocker)
+    extractor.extract_next_in_queue()
+    assert capfd.readouterr()[0] == ''
